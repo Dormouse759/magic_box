288db239452780ebcbb1e99c26d797ea5a02c905
kratas <kratas123@seznam.cz>
2018-10-23 19:01:10 +0200

    Oprava chyb v new_measure

--- new_measure.py
+++ new_measure.py
@@ -6,36 +6,54 @@ global measure_proc
 
 
 def measurement(action):
-	if action is "start"
+	global measure_proc
+
+	if action is "start":
 		measure_proc=subprocess.Popen('utils/generator', stdout=subprocess.PIPE)
-		if measure_proc.poll() is None
+		if measure_proc.poll() is None:
 			return True
 		
-	if action is "stop"
-		if measure_proc.poll() is not None
+		else:
+			return False
+		
+	if action is "stop":
+		if measure_proc.poll() is not None:
 			measure_proc.terminate()
-				if measure_proc.poll() is not None
+				if measure_proc.poll() is not None:
 					return True
+
+		else:
+			return False
+
 def is_running():
+	global measure_proc
+
 	if measure_proc is None:
-	return False
+		return False
+
 	else:
-	return True
+		return True
 
 #Vraci aktualni cas, pokud se povede spustit vlakno, jinak 'None'
 def mstart():
+	global measure_proc
+
 	if measure_proc is None:
 		start=measurement("start")
-		if start is True
+		if start is True:
 			return mylibrary.get_actual_time()
+
 		else:
 			return None
 
 #Vraci aktualni cas, pokud se povede ukoncit vlakno, jinak 'None'
 def mstop():
+	global measure_proc
+
 	if measure_proc is not None:
 		stop=measurement("stop")
-		if stop is True
+		if stop is True:
 			return mylibrary.get_actual_time()
+		
 		else:
 			return None
